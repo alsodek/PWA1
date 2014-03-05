36c3bae258b2738bd01f27e1acd742cf6ad99220
alsodek <alansodek@alans-mbp.att.net>
2014-03-05 15:26:17 -0600

    initial setup

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -2,3 +2,36 @@
 // March 5, 2014
 // The Duel - Part I
 
+//self-executing function
+(function(){
+
+	console.log("FIGHT!!!");
+
+	//player name
+	var playerOneName = "Spiderman";
+	var playerTwoName = "Batman";
+	
+	//player damage
+	var player1Damage = 20;
+	var player2Damage = 20;
+	
+	//player health
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
+	
+	var round = 1;
+
+	function fight(){
+		console.log("in the fight function");
+		
+	};
+	
+	function winnerCheck(){
+		//code will go here
+	};
+	
+	/********** The program gets started below **********/
+	console.log("program starts");
+	fight();
+
+})();
