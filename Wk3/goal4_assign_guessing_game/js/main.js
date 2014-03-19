cbdd0473156c40e87c6231aaa83817e1f3afbf6a
alsodek <alansodek@alans-mbp.att.net>
2014-03-19 18:17:40 -0500

    completed game

--- Wk3/goal4_assign_guessing_game/js/main.js
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -1,14 +1,100 @@
 /**
  * GUESSING GAME:
  *
- * Created By: 
- * Date: 
+ * Created By: Alan Sodek
+ * Date: March 18, 2014
  * 
  * GUESSING GAME
  */
 
 //Game variables
-(function ()
+(function (){
 
+	var playersGuess = 0;
+	var guessesRemaining = 3;
+	var guessesMade = 0;
+	
+	//Define dom elements.
+	var dom = {
+		input: document.querySelector("#input"),
+		output: document.querySelector("#output"),
+		button: document.querySelector("button")		
+	};
+	
+	//Computer picks random number. 
+	var magicNum = Math.floor((Math.random() * 10) + 1);
+	
+	//Each time user clicks, console remaining guesses and call validateInput.
+	var clickFn = function(e){
+		console.log(guessesRemaining);
+		validateInput();
+	};
+	
+	//validate user guess/input and call playGame.
+	var validateInput = function(){
+		playersGuess = parseInt(dom.input.value);
+		
+		if(isNaN(playersGuess)){
+			dom.output.innerHTML = "Please enter a number.";
+			
+		}else if(playersGuess < 1 || playersGuess > 10){
+			dom.output.innerHTML = "Please enter a number between 1 and 10.";
+		
+		}else{
+			playGame();
+		};
+	};	
+		
+	//Play the game.
+	var playGame = function(){
+		guessesRemaining--;
+		guessesMade++;
+		gameState = "Guess " + guessesMade + ", Remaining: " + guessesRemaining;
+		playerGuess = parseInt(input.value);
+		
+		if(playerGuess > magicNum){
+			dom.output.innerHTML = "That's too high." + gameState;
+			
+		}else if (playersGuess < magicNum){
+			dom.output.innerHTML = "That's too low." + gameState;
+			
+		}else if (playersGuess === magicNum){
+			gameOver(true);
+		};
+		
+		if(guessesRemaining < 1){
+			console.log("Game Over : guessesRemaining is < 1");
+			gameOver(false);
+		};
+	};
+	
+	var gameOver = function(win){
+		
+		if(win){
+			dom.output.innerHTML
+				= "Yes, it's " + magicNum + "!" + "<br>"
+				+ "It only took you " + guessesMade + " guesses.";
+		}else{
+			dom.output.innerHTML
+				= "No more guesses left!" + "<br>"
+				+ "The number was: " + magicNum + ".";
+		};
+		
+		//Disable the button
+		dom.button.removeEventListener("click", clickFn, false);
+		
+	};
+	
+	var onKeyDown = function(e){
+		
+		if(e.keyCode === 13){
+			validateInput();
+		};
+	};
+	
+	console.log(magicNum);
+	
+	//addEventListener method
+	dom.button.addEventListener("click", clickFn, false);
 
 })();
